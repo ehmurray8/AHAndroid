c82ad33540759466709cb795b0bc2f66492dbe1b
ehmurray8 <ehmurray8@gmail.com>
2017-09-03 03:25:15 -0400

    Added TV Remote.

--- app/src/main/java/ehmsoftware/ahandroid/SocketRemoteActivity.java
+++ app/src/main/java/ehmsoftware/ahandroid/SocketRemoteActivity.java
@@ -1,83 +1,92 @@
 package ehmsoftware.ahandroid;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import io.ably.lib.realtime.AblyRealtime;
-import io.ably.lib.realtime.Channel;
-import io.ably.lib.realtime.CompletionListener;
-import io.ably.lib.types.AblyException;
-import io.ably.lib.types.ErrorInfo;
-import io.ably.lib.types.Message;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class SocketRemoteActivity extends AppCompatActivity {
 
-    private static final String API_KEY = "NtYGvg.lADLCg:_YcwQxp-lniZ_xWY";
-    private AblyRealtime realtime;
-    public static String CHAN_STR = "AH_Test";
+    private enum SockState {
+        ON,
+        OFF;
+
+        public String toString() {
+            return this.name().toLowerCase();
+        }
+    }
+
+    private enum SocketInfo {
+
+        SOCK1_ON("Socket 1 On", R.id.sock1_on, 1, SockState.ON),
+        SOCK1_OFF("Socket 1 Off", R.id.sock1_off, 1, SockState.OFF),
+        SOCK2_ON("Overhead Light On", R.id.sock2_on, 2, SockState.ON),
+        SOCK2_OFF("Overhead Light Off", R.id.sock2_off, 2, SockState.OFF),
+        SOCK3_ON("Fan On", R.id.sock3_on, 3, SockState.ON),
+        SOCK3_OFF("Fan Off", R.id.sock3_off, 3, SockState.OFF),
+        SOCK4_ON("AC On", R.id.sock4_on, 4, SockState.ON),
+        SOCK4_OFF("AC Off", R.id.sock4_off, 4, SockState.OFF),
+        SOCK5_ON("Lamp On", R.id.sock5_on, 5, SockState.ON),
+        SOCK5_OFF("Lamp Off", R.id.sock5_off, 5, SockState.OFF);
+
+        private String toastString;
+        private int btnId;
+        private int sockNum;
+        private SockState sockState;
+
+        SocketInfo(String toastString, int btnId, int sockNum, SockState sockState) {
+            this.toastString = toastString;
+            this.btnId = btnId;
+            this.sockNum = sockNum;
+            this.sockState = sockState;
+        }
+
+        public String getToastString() {
+            return this.toastString;
+        }
+
+        public int getId() {
+            return this.btnId;
+        }
+
+        public int getSockNum() {
+            return this.sockNum;
+        }
+
+        public SockState getSockState() {
+            return this.sockState;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_socket_remote);
 
-        int[] socketIds = {R.id.sock1_on, R.id.sock1_off, R.id.sock2_on, R.id.sock2_off,
-                           R.id.sock3_on, R.id.sock3_off, R.id.sock4_on, R.id.sock4_off,
-                           R.id.sock5_on, R.id.sock5_off};
-
-        String[] toast_strs = {"Socket 1 On", "Socket 1 Off", "Lamp On", "Lamp Off",
-                               "Fan On", "Fan Off", "AC On", "AC Off", "Overhead Light On",
-                               "Overhead Light Off"};
-
-        try {
-            initAbly();
-        } catch(AblyException e) {
-            e.printStackTrace();
+        for(SocketInfo si : SocketInfo.values()) {
+            Button button = (Button) findViewById(si.getId());
+            new ButtonClick(si, this, button);
         }
-
-        for(int i = 0; i < socketIds.length; i++) {
-            Button button = (Button) findViewById(socketIds[i]);
-            new ButtonClick(toast_strs[i], button, this, this.realtime);
-        }
-    }
-
-    private void initAbly() throws AblyException {
-        this.realtime = new AblyRealtime(API_KEY);
-
-        Channel channel = realtime.channels.get(CHAN_STR);
-        channel.subscribe(new Channel.MessageListener() {
-            @Override
-            public void onMessage(Message message) {
-                //Toast.makeText(getBaseContext(), "Message received: " + message.data,
-                //        Toast.LENGTH_SHORT).show();
-                Log.v("ABLY", "Message received: " + message.data);
-            }
-        });
     }
 
     private static class ButtonClick implements View.OnClickListener {
 
-        private String toastString;
-        private Button button;
+        private SocketInfo socketInfo;
         private Context context;
-        private Channel channel;
-
         private static Toast currToast = null;
 
-        public ButtonClick(String toastString, Button button, Context context,
-                           AblyRealtime realtime) {
-            this.toastString = toastString;
-            this.button = button;
+        public ButtonClick(SocketInfo socketInfo, Context context,  Button button) {
+            this.socketInfo = socketInfo;
             this.context = context;
-            this.channel = realtime.channels.get(SocketRemoteActivity.CHAN_STR);
 
-
-            this.button.setOnClickListener(this);
+            button.setOnClickListener(this);
         }
 
         @Override
@@ -86,24 +95,37 @@ public class SocketRemoteActivity extends AppCompatActivity {
                 currToast.cancel();
             }
 
-            try {
-                this.channel.publish("update", toastString, new CompletionListener() {
-                    @Override
-                    public void onSuccess() {
-                        Log.d("ABLY", "Message Sent");
-                    }
+            new PostMessage().execute(this.socketInfo);
 
-                    @Override
-                    public void onError(ErrorInfo reason) {
-                        Log.d("ABLY", "Error sending message.");
-                    }
-                });
-            } catch (AblyException e) {
+            currToast = Toast.makeText(this.context, this.socketInfo.getToastString(), Toast.LENGTH_SHORT);
+            currToast.show();
+        }
+    }
+
+    private static class PostMessage extends AsyncTask<SocketInfo, Void, HttpResponse<String>> {
+
+
+        protected HttpResponse<String> doInBackground(SocketInfo... socketInfo) {
+
+            HttpResponse<String> response = null;
+            try {
+                 response = Unirest.post("https://rest.ably.io/channels/" + AblyConstants.CHAN_STR
+                                + "/messages")
+                        .header("content-type", "application/json")
+                        .header("authorization", AblyConstants.ABLY_AUTH)
+                        .header("cache-control", "no-cache")
+                        .body("{\n\t\"name\":\"Test\",\n\t\"data\": {\"Function Name\":\"Socket\",\"Socket Type\":\"" +
+                                socketInfo[0].getSockNum() +"\",\"Socket State\":\"" +
+                                socketInfo[0].getSockState().toString() + "\"}\n}")
+                        .asString();
+            } catch (UnirestException e) {
                 e.printStackTrace();
             }
+            return response;
+        }
 
-            currToast = Toast.makeText(this.context, this.toastString, Toast.LENGTH_SHORT);
-            currToast.show();
+        protected void onPostExecute(HttpResponse<String> response) {
+
         }
     }
 }
--- app/src/main/java/ehmsoftware/ahandroid/TVRemoteActivity.java
+++ app/src/main/java/ehmsoftware/ahandroid/TVRemoteActivity.java
@@ -1,94 +1,119 @@
 package ehmsoftware.ahandroid;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import io.ably.lib.realtime.AblyRealtime;
-import io.ably.lib.realtime.Channel;
-import io.ably.lib.realtime.CompletionListener;
-import io.ably.lib.types.AblyException;
-import io.ably.lib.types.ErrorInfo;
-import io.ably.lib.types.Message;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class TVRemoteActivity extends AppCompatActivity {
 
-    private static final String API_KEY = "NtYGvg.lADLCg:_YcwQxp-lniZ_xWY";
-    private AblyRealtime realtime;
-    public static String CHAN_STR = "AH_Test";
+    private enum Key {
+
+        POWER("POWER", R.id.power_btn),
+        INPUT("MEDIA", R.id.input_btn),
+        ONE("1", R.id.btn_1),
+        TWO("2", R.id.btn_2),
+        THREE("3", R.id.btn_3),
+        FOUR("4", R.id.btn_4),
+        FIVE("5", R.id.btn_5),
+        SIX("6", R.id.btn_6),
+        SEVEN("7", R.id.btn_7),
+        EIGHT("8", R.id.btn_8),
+        NINE("9", R.id.btn_9),
+        DOT("DOT", R.id.btn_dot),
+        ZERO("0", R.id.btn_0),
+        MENU("MENU", R.id.menu_btn),
+        UP("UP", R.id.up_arrow_btn),
+        HOME("HOME", R.id.home_btn),
+        LEFT("LEFT", R.id.left_arrow_btn),
+        ENTER("ENTER", R.id.enter_btn),
+        RIGHT("RIGHT", R.id.right_arrow_btn),
+        EXIT("EXIT", R.id.exit_btn),
+        DOWN("DOWN", R.id.down_arrow_btn),
+        INFO("INFO", R.id.info_btn),
+        VOL_UP("VOLUME UP", R.id.vol_up_btn),
+        CHAN_UP("CHANNEL UP", R.id.chan_up_btn),
+        MUTE("MUTE", R.id.mute_btn),
+        VOL_DOWN("VOLUME DOWN", R.id.vol_down_btn),
+        CHAN_DOWN("CHANNEL DOWN", R.id.chan_down_btn);
+
+        private String key;
+        private int btnId;
+
+        Key(String key, int btnId) {
+            this.key = key;
+            this.btnId = btnId;
+        }
+
+        public int getId() {
+            return this.btnId;
+        }
+
+        public String getKey() {
+            return this.key;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tvremote);
 
-        try {
-            initAbly();
-        } catch(AblyException e) {
-            e.printStackTrace();
+        for(Key k : Key.values()) {
+            Button button = (Button) findViewById(k.getId());
+            new ButtonClick(k, this, button);
         }
-    }
-
-    private void initAbly() throws AblyException {
-        this.realtime = new AblyRealtime(API_KEY);
-
-        Channel channel = realtime.channels.get(CHAN_STR);
-        channel.subscribe(new Channel.MessageListener() {
-            @Override
-            public void onMessage(Message message) {
-                Log.v("ABLY", "Message received: " + message.data);
-            }
-        });
     }
 
     private static class ButtonClick implements View.OnClickListener {
 
-        private String toastString;
+        private Key key;
         private Button button;
         private Context context;
-        private Channel channel;
 
-        private static Toast currToast = null;
-
-        public ButtonClick(String toastString, Button button, Context context,
-                           AblyRealtime realtime) {
-            this.toastString = toastString;
+        public ButtonClick(Key key, Context context, Button button) {
+            this.key = key;
             this.button = button;
             this.context = context;
-            this.channel = realtime.channels.get(TVRemoteActivity.CHAN_STR);
-
 
             this.button.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            if(currToast != null) {
-                currToast.cancel();
-            }
+            new PostMessage().execute(this.key);
+        }
+    }
 
+    private static class PostMessage extends AsyncTask<Key, Void, HttpResponse<String>> {
+
+
+        protected HttpResponse<String> doInBackground(Key... socketInfo) {
+
+            HttpResponse<String> response = null;
             try {
-                this.channel.publish("update", toastString, new CompletionListener() {
-                    @Override
-                    public void onSuccess() {
-                        Log.d("ABLY", "Message Sent");
-                    }
-
-                    @Override
-                    public void onError(ErrorInfo reason) {
-                        Log.d("ABLY", "Error sending message.");
-                    }
-                });
-            } catch (AblyException e) {
+                response = Unirest.post("https://rest.ably.io/channels/" + AblyConstants.CHAN_STR
+                                + "/messages")
+                        .header("content-type", "application/json")
+                        .header("authorization", AblyConstants.ABLY_AUTH)
+                        .header("cache-control", "no-cache")
+                        .body("{\n\t\"name\":\"Test\",\n\t\"data\": {\"Function Name\":\"Key\",\"Key Type\":\"" +
+                                socketInfo[0].getKey() +"\"}\n}")
+                        .asString();
+            } catch (UnirestException e) {
                 e.printStackTrace();
             }
+            return response;
+        }
 
-            currToast = Toast.makeText(this.context, this.toastString, Toast.LENGTH_SHORT);
-            currToast.show();
+        protected void onPostExecute(HttpResponse<String> response) {
+
         }
     }
 }
